beffe280ebe0c8078131cbea634e0dee07a739b9
Aaron <kleiaaro@gmail.com>
2025-04-01 16:42:45 +0200

    init searcher

---
+++ open_optformer/history.py
@@ -0,0 +1,61 @@
+import json
+
+from dataclasses import dataclass, field
+
+from syne_tune.config_space import Categorical, Float, Integer
+
+
+@dataclass
+class Trial:
+    config: dict
+    metric: int
+
+
+@dataclass
+class Study:
+    name: str
+    algorithm: str
+    config_space: dict
+    metric_names: list = field(default_factory=list)
+    trials: list = field(default_factory=list)
+
+    def add_trial(self, config, result):
+
+        trial = Trial(config, result)
+        self.trials.append(trial)
+
+    def get_prompt(self):
+        string = f"benchmark:{self.name},"
+        string += f"algorithm:{self.algorithm},"
+
+        for hp_name, hp in self.config_space.items():
+            string += f"parameter:"
+            string += "{"
+            string += f"name:{hp_name}, "
+
+            if isinstance(hp, Categorical):
+
+                string += f"type:CAT,"
+                string += f"categories:{hp.categories},"
+            elif isinstance(hp, Float):
+                    string += f"type:UNI,"
+                    string += f"min_value:{hp.lower},"
+                    string += f"max_value:{hp.upper},"
+            elif isinstance(hp, Integer):
+                    string += f"type:Int,"
+                    string += f"min_value:{hp.lower},"
+                    string += f"max_value:{hp.upper},"
+            string += "}"
+
+        string += '&'
+        for trial in self.trials:
+#            string += "trial:{"
+            for i, hp in enumerate(self.config_space):
+                if i > 0:
+                    string += ","
+                string += str(trial.config[hp])
+            string += f"*"
+            string += f"{trial.metric}"
+            string += f"|"
+ #           string += "},"
+        return string
---
+++ open_optformer/optformer_searcher.py
@@ -0,0 +1,183 @@
+import logging
+
+from typing import Optional, List, Dict, Any
+from pathlib import Path
+
+import torch
+from litgpt.config import Config
+from litgpt.tokenizer import Tokenizer
+from litgpt.model import GPT
+
+from open_optformer.history import Study
+
+from syne_tune.config_space import Integer, Categorical, Float
+from syne_tune.optimizer.schedulers.searchers.single_objective_searcher import SingleObjectiveBaseSearcher
+
+logger = logging.getLogger(__name__)
+
+
+def preprocess(prompt: str):
+    prompt = prompt.replace('parameter', "")
+    prompt = prompt.replace('trial', "")
+    prompt = prompt.replace('\"', "")
+    prompt = prompt.replace(' ', "")
+    return prompt
+
+
+
+def select_token(logits, pos):
+    #m = logits[:, pos].argmax(dim=-1)
+    probs = torch.nn.functional.softmax(logits[:, pos], dim=-1).detach().numpy()[0, :]
+    m = np.random.choice(np.arange(pos.shape[0]), p=probs)
+    token = pos[m]
+    return token
+
+
+class OptFormerSearcher(SingleObjectiveBaseSearcher):
+    """
+
+    :param config_space: Configuration space
+    :param points_to_evaluate: List of configurations to be evaluated
+        initially (in that order). Each config in the list can be partially
+        specified, or even be an empty dict. For each hyperparameter not
+        specified, the default value is determined using a midpoint heuristic.
+        If ``None`` (default), this is mapped to ``[dict()]``, a single default config
+        determined by the midpoint heuristic. If ``[]`` (empty list), no initial
+        configurations are specified.
+    """
+
+    def __init__(
+        self,
+        checkpoint_dir: Path,
+        config_space: Dict[str, Any],
+        task_info: Dict = None,
+        points_to_evaluate: Optional[List[Dict[str, Any]]] = None,
+        random_seed: int = None,
+    ):
+        super().__init__(config_space, points_to_evaluate, random_seed)
+
+        config = Config.from_file(str(checkpoint_dir / 'model_config.yaml'))
+        self.model = GPT(config)
+
+#        self.tokenizer = Tokenizer(str(Path(__file__).parent / "data" / "tokenizer"))
+        self.tokenizer = Tokenizer(str(checkpoint_dir))
+        self.model.load_state_dict(torch.load(str(checkpoint_dir / 'lit_model.pth'), weights_only=True)['model'])
+        self.history = []
+
+        if task_info is None:
+            self.task_info = {'name': "tst",
+                              "algorithm": "BORE",
+                              "metric_names": "error"}
+        else:
+            self.task_info = task_info
+
+        self.study = Study(config_space=config_space,
+                           name=self.task_info['name'],
+                           algorithm=self.task_info['algorithm'],
+                           metric_names=[self.task_info['metric_names']],
+        )
+
+    def suggest(self, **kwargs) -> Optional[Dict[str, Any]]:
+        """Suggest a new configuration.
+
+        Note: Query :meth:`_next_initial_config` for initial configs to return
+        first.
+
+        :param kwargs: Extra information may be passed from scheduler to
+            searcher
+        :return: New configuration. The searcher may return None if a new
+            configuration cannot be suggested. In this case, the tuning will
+            stop. This happens if searchers never suggest the same config more
+            than once, and all configs in the (finite) search space are
+            exhausted.
+
+        """
+
+        prompt = preprocess(self.study.get_prompt())
+        token = self.tokenizer.encode(prompt)
+        prompt_size = token.size(0)
+        input_pos = torch.arange(0, token.size(0))
+        input_pos_maxp1 = torch.tensor([prompt_size])
+
+        self.model.set_kv_cache(batch_size=1)
+        prefill_token = True
+        config = {}
+
+        for hp_name, hp in self.config_space.items():
+            logits = self.model(token.view(1, -1), input_pos, input_pos_maxp1=input_pos_maxp1)[:, -1]
+
+            if isinstance(hp, Float) or isinstance(hp, Integer):
+                # pick value in [0, Q] with the highest probability
+                idx = torch.tensor([self.tokenizer.encode(str(i))[-1] for i in range(1000)], dtype=torch.int)
+                m = logits[:, idx].argmax(dim=-1)
+                token_id = idx[m]
+                token = int(self.tokenizer.decode(token_id))
+                config[hp_name] = token / 1000 * (hp.upper - hp.lower) + hp.lower
+
+            elif isinstance(hp, Categorical):
+                #  pick the category with the highest probability
+                idx = torch.tensor([self.tokenizer.encode(str(cat))[-1] for cat in hp.categories], dtype=torch.int)
+                token = select_token(logits, idx)
+                value = int(self.tokenizer.decode(token))
+                config[hp_name] = hp.categories[value]
+            if prefill_token:
+                prefill_token = False
+                input_pos = torch.tensor([prompt_size],dtype=torch.int64)
+            else:
+                input_pos.add_(1)
+            input_pos_maxp1.add_(1)
+            self.model(token.view(1, -1), input_pos, input_pos_maxp1=input_pos_maxp1)
+            input_pos.add_(1)
+            token = torch.tensor([1012])
+        return config
+
+    def on_trial_complete(
+            self,
+            trial_id: int,
+            config: Dict[str, Any],
+            metric: float,
+            resource_level: int = None,
+    ):
+        """Inform searcher about result
+
+        The scheduler passes every result. If ``update == True``, the searcher
+        should update its surrogate model (if any), otherwise ``result`` is an
+        intermediate result not modelled.
+
+        The default implementation calls :meth:`_update` if ``update == True``.
+        It can be overwritten by searchers which also react to intermediate
+        results.
+
+        :param trial_id: See :meth:`~syne_tune.optimizer.schedulers.TrialScheduler.on_trial_result`
+        :param config: See :meth:`~syne_tune.optimizer.schedulers.TrialScheduler.on_trial_result`
+        :param metric: See :meth:`~syne_tune.optimizer.schedulers.TrialScheduler.on_trial_result`
+        """
+        self.study.add_trial(config, metric)
+
+
+    def on_trial_error(self, trial_id: int):
+        """Called by scheduler if an evaluation job for a trial failed.
+
+        The searcher should react appropriately (e.g., remove pending evaluations
+        for this trial, not suggest the configuration again).
+
+        :param trial_id: ID of trial whose evaluated failed
+        """
+        return
+
+if __name__ == '__main__':
+
+    import numpy as np
+    import pathlib
+    from syne_tune.config_space import randint, choice
+
+    config_space = {"a": choice([0, 1, 2, 3, 4])}
+
+    checkpoint_dir = pathlib.Path(__file__).parent / "models" / "small_custom_model" / "step-00000800"
+    searcher = OptFormerSearcher(config_space=config_space, checkpoint_dir=checkpoint_dir)
+
+    trial_id = 0
+    config = searcher.suggest()
+    print(config)
+    metric = np.random.rand()
+    searcher.on_trial_complete(trial_id, config, metric)
