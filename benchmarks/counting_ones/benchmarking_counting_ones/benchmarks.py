b65bbeb24493d2a946f07b68537b6557130f5985
Aaron Klein <kleiaaro@gmail.com>
2025-06-23 11:01:54 +0200

    add 5D, 10D, 20D

--- benchmarks/counting_ones/benchmarking_counting_ones/benchmarks.py
+++ benchmarks/counting_ones/benchmarking_counting_ones/benchmarks.py
@@ -13,8 +13,18 @@ class BenchmarkDefinition:
     max_num_evaluations: Optional[int] = None
 
 benchmark_definitions = {
-    "co-5": BenchmarkDefinition(objective='counting_ones_5D.py',
+    "co-5": BenchmarkDefinition(objective='benchmarking_counting_ones/counting_ones_5D.py',
                                 dim=5,
+                                metric="feval",
+                                mode='max',
+                                max_num_evaluations=20),
+    "co-10": BenchmarkDefinition(objective='benchmarking_counting_ones/counting_ones_10D.py',
+                                dim=10,
+                                metric="feval",
+                                mode='max',
+                                max_num_evaluations=20),
+    "co-20": BenchmarkDefinition(objective='benchmarking_counting_ones/counting_ones_20D.py',
+                                dim=20,
                                 metric="feval",
                                 mode='max',
                                 max_num_evaluations=20)
