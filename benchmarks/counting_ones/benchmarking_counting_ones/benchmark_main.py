1dd8d1aa410ffae449c320c7913b67e3f7fcc7a1
Aaron Klein <kleiaaro@gmail.com>
2025-07-15 15:37:46 +0200

    fix initial point

--- benchmarks/counting_ones/benchmarking_counting_ones/benchmark_main.py
+++ benchmarks/counting_ones/benchmarking_counting_ones/benchmark_main.py
@@ -56,7 +56,7 @@ def run(
         random_state = np.random.RandomState(seed)
         points_to_evaluate = [
             {
-                k: v.sample(random_state=random_state) if hasattr(v, "sample") else v
+                k: 0 if hasattr(v, "sample") else v
                 for k, v in config_space.items()
             }
             for _ in range(num_random_candidates)
