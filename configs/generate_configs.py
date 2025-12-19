a0b7c23ec3c8ff8da75f896edb4f5db4fd6861cc
Aaron Klein <kleiaaro@gmail.com>
2025-12-19 16:54:09 +0100

    sweeps

--- configs/generate_configs.py
+++ configs/generate_configs.py
@@ -1,7 +1,9 @@
+import os
+
 import yaml
 from pathlib import Path
 
-BASE_PATH_CLUSTER = '/data/horse/ws/aakl689g-optformer/syne-tune-benchmarks'
+BASE_PATH_CLUSTER = os.environ['BASE_PATH']
 DATASET_NAME = 'all'
 WANDB_PROJECT = 'open_optformer_qwen3_hp_sweep'
 
@@ -28,37 +30,32 @@ def generate_configs():
             '8B': 8_000_000_000,
             '10B': 10_000_000_000,
         }
-        lr_grid = {
-            "1e-5": 1e-5,
-            "5e-5": 3e-5,
-            "1e-4": 1e-4,
-            "5e-4": 3e-4,
-            "1e-3": 1e-3,
-        }
+        lr_grid = [1e-5, 5e-5, 1e-4, 5e-4, 1e-3]
 
-        bsz_grid = {
-            "32": 32,
-            "64": 64,
-            "128": 128,
-            "256": 256,
-            "512": 512,
-        }
-
+        gas_grid = [1, 2, 4, 8, 16]
+        mbs = 16
         base_path = Path(BASE_PATH_CLUSTER)
 
         for name, tokens in token_counts.items():
             for lr in lr_grid:
-                for bsz in bsz_grid
+                for gas in gas_grid:
+
+                    bsz = int(gas * mbs)
+                    number_of_steps = tokens // (bsz * base_config['train']['max_seq_length'])
                     new_config = base_config.copy()
                     new_config['train']['max_tokens'] = tokens
                     new_config['optimizer']['init_args']['lr'] = lr
                     new_config['train']['global_batch_size'] = bsz
+                    new_config['train']['log_interval'] = number_of_steps // 500
+                    new_config['train']['lr_warmup_steps'] = int(number_of_steps * 0.05)  # 5% warmup
+                    new_config['train']['micro_batch_size'] = mbs
+                    new_config['eval']['interval'] = number_of_steps // 500
                     run_name = f"{model_name}_token_{name}_lr_{lr}_bsz_{bsz}"
                     new_config['log']['run'] = run_name
 
-                    new_config['data']['init_args']['data_path'] = str(base_path / 'data' / 'tokenized_data' / DATASET_NAME)
+                    new_config['data']['init_args']['data_path'] = str(base_path / 'tokenized_dataset' / DATASET_NAME)
                     new_config['tokenizer_dir'] = str(base_path / 'tokenizer')
-                    new_config['out_dir'] = str(base_path / run_name)
+                    new_config['out_dir'] = str(base_path / 'checkpoints' / run_name)
 
                     new_config['log']['project'] = WANDB_PROJECT
 
